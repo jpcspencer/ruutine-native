e723a472436899b01dca06812790079ac616a3a9
Jordan Spencer <crearysp@gmail.com>
2026-06-06 12:53:43 +0400

    Initial Commit

---
+++ Ruutine/RuutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuutineApp.swift
+//  Ruutine
+//
+//  Created by Jordan Spencer on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct RuutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
